8a83e19203303e5f3a5dd91f7cafcb30593e702d
GabrielAs15 <bielsantos200510@gmail.com>
2022-05-30 18:14:58 -0300

    prova4 - segunda

---
+++ API/src/index.js
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+
+import express from 'express'
+import cors from 'cors'
+import { con } from './repository/connection.js'
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+
+
+
+
+
+
+server.listen(process.env.PORT, () => console.log(`API Conectada na Porta ${process.env.PORT}`))
